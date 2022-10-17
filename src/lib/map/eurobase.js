4e463e7e77d9b52d2701b75beb29fcba75f1cb3a
Ben Cannell <bencannell@Bens-MacBook-Air.local>
2022-10-17 10:15:06 +0100

    Updates for geojson and added gpx files

--- src/lib/map/eurobase.js
+++ src/lib/map/eurobase.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import { MapContainer, TileLayer, Pane, GeoJSON, Marker, Popup } from 'react-leaflet'
 
 const EuroMap = (props) => {
-  const [euCountries, setEuCountries] = useState([]);
+  const [euCountries, setEuCountries] = useState();
   const cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
 
   const getData=()=>{
@@ -15,11 +15,9 @@ const EuroMap = (props) => {
     }
     )
       .then(function(response){
-        console.log(response)
-        return response.json();
+        return response.json()
       })
       .then(function(myJson) {
-        console.log(myJson);
         setEuCountries(myJson)
       });
   }
@@ -34,45 +32,17 @@ const EuroMap = (props) => {
         attribution={cartodbAttribution}
         url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png"
       />
-      <TileLayer
+      {/* <TileLayer
         attribution={cartodbAttribution}
         url="https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png"
         pane="labels"
-      />
-      <GeoJSON data={euCountries}></GeoJSON>
+      /> */}
+      {euCountries && (
+        <GeoJSON attribution="test attribution" data={euCountries}/>
+      )}
       
     </MapContainer>
   )
 }
 
 export {EuroMap}
-
-// var map = L.map('map');
-
-// map.createPane('labels');
-
-// // This pane is above markers but below popups
-// map.getPane('labels').style.zIndex = 650;
-
-// // Layers in this pane are non-interactive and do not obscure mouse/touch events
-// map.getPane('labels').style.pointerEvents = 'none';
-
-// var cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
-
-// var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-//   attribution: cartodbAttribution
-// }).addTo(map);
-
-// var positronLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
-//   attribution: cartodbAttribution,
-//   pane: 'labels'
-// }).addTo(map);
-
-// /* global euCountries */
-// var geojson = L.geoJson(euCountries).addTo(map);
-
-// geojson.eachLayer(function (layer) {
-//   layer.bindPopup(layer.feature.properties.name);
-// });
-
-// map.setView({lat: 47.040182144806664, lng: 9.667968750000002}, 4);
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
 const WebpackDashDynamicImport = require('@plotly/webpack-dash-dynamic-import');
 const packagejson = require('./package.json');
-const { join } = require('path');
-const staticPath = join(process.cwd(), '/assets');
-console.log(staticPath)
-const { WebpackPluginServe: Serve } = require('webpack-plugin-serve');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const dashLibraryName = packagejson.name.replace(/-/g, '_');
 
@@ -122,7 +119,11 @@ module.exports = (env, argv) => {
                 filename: '[file].map',
                 exclude: ['async-plotlyjs']
             }),
-            new Serve({ static: staticPath, port:8000 })
+            new CopyWebpackPlugin([ { 
+                // patterns: [
+                "from": "./assets", "to": "assets"
+                // ]
+            }])
         ]
     }
 };
