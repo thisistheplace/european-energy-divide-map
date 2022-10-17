3dfa95e0c69a12fddd1dcd0492ce6a55802f863f
Ben Cannell <bencannell@Bens-MacBook-Air.local>
2022-10-17 13:37:57 +0100

    Update for build

--- src/EuropeanEnergyDivideMap.jl
+++ src/EuropeanEnergyDivideMap.jl
@@ -3,7 +3,7 @@ module EuropeanEnergyDivideMap
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.0.1"
+const version = "0.0.2"
 
 include("jl/europeanenergydividemap.jl")
 
@@ -16,14 +16,14 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "european_energy_divide_map.min.js",
-    external_url = "https://unpkg.com/european_energy_divide_map@0.0.1/european_energy_divide_map/european_energy_divide_map.min.js",
+    external_url = "https://unpkg.com/european_energy_divide_map@0.0.2/european_energy_divide_map/european_energy_divide_map.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "european_energy_divide_map.min.js.map",
-    external_url = "https://unpkg.com/european_energy_divide_map@0.0.1/european_energy_divide_map/european_energy_divide_map.min.js.map",
+    external_url = "https://unpkg.com/european_energy_divide_map@0.0.2/european_energy_divide_map/european_energy_divide_map.min.js.map",
     dynamic = true,
     async = nothing,
     type = :js
